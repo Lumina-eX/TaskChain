b57e00a59c3886c1925dfa8829fbddb2dc24b96b
Stephan-Thomas <xevermontes8@gmail.com>
2026-02-24 12:24:36 +0100

    Fix: Implemented a modern light mode using the existing theme token system

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link'
 import { Button } from '@/components/ui/button'
 import { Menu, X } from 'lucide-react'
 import { useState } from 'react'
+import { ThemeToggle } from './ui/ThemeToggle'
 
 export function Navbar() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
@@ -39,6 +40,7 @@ export function Navbar() {
             <Button asChild>
               <Link href="/signup">Get Started</Link>
             </Button>
+            <ThemeToggle />
           </div>
 
           <button
